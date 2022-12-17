938caa91bceac59796e120143b21e04fe42bf0a3
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-12-17 15:51:08 +0700

    add conditional rendering to highlight opened menu

--- app/Views/components/Navbar.php
+++ app/Views/components/Navbar.php
@@ -1,5 +1,6 @@
 <?php
 $session = session();
+$uri = service('uri');
 ?>
 <!-- Header Section Begin -->
 <header class="header-section">
@@ -74,11 +75,19 @@ $session = session();
       </div>
       <nav class="nav-menu mobile-menu">
         <ul>
-          <li class="active"><a href="<?= site_url("/") ?>">Home</a></li>
-          <li><a href="<?= site_url("shop") ?>">Shop</a></li>
-          <li><a href="<?= site_url("contact") ?>">Contact</a></li>
+          <li <?= ($uri->getSegment(1) == '') ? 'class="active"' : '' ?>>
+            <a href="<?= site_url("/") ?>">Home</a>
+          </li>
+          <li <?= ($uri->getSegment(1) == 'shop') ? 'class="active"' : '' ?>>
+            <a href="<?= site_url("shop") ?>">Shop</a>
+          </li>
+          <li <?= ($uri->getSegment(1) == 'contact') ? 'class="active"' : '' ?>>
+            <a href="<?= site_url("contact") ?>">Contact</a>
+          </li>
           <?php if ($session->get("isLoggedIn")) : ?>
-            <li><a href="<?= site_url("transaction") ?>">Transaction</a></li>
+            <li <?= ($uri->getSegment(1) == 'transaction') ? 'class="active"' : '' ?>>
+              <a href="<?= site_url("transaction") ?>">Transaction</a>
+            </li>
           <?php endif ?>
         </ul>
       </nav>
