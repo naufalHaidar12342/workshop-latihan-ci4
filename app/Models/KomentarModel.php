25ba5612c91199bad5135dfd68efa85851488a6b
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-12-17 15:31:31 +0700

    tidy up member of allowedFields array inside KomentarModel

--- app/Models/KomentarModel.php
+++ app/Models/KomentarModel.php
@@ -9,7 +9,13 @@ class KomentarModel extends Model
     protected $table = 'komentar';
     protected $primaryKey = 'id';
     protected $allowedFields = [
-        'id_barang', 'id_user', 'komentar', 'created_date', 'created_by', 'updated_date', 'updated_by'
+        'id_barang',
+        'id_user',
+        'komentar',
+        'created_date',
+        'created_by',
+        'updated_date',
+        'updated_by'
     ];
     protected $returnType = 'App\Entities\Komentar';
     protected $useTimestamps = false;
