082f80a7f3e286d5525c22052dabcc37c8de68a4
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-12-22 19:41:59 +0700

    change h1 content to promo code

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -9,7 +9,7 @@
                     <div class="row">
                         <div class="col-lg-5">
                             <span>All Product</span>
-                            <h1>Winter</h1>
+                            <h1><?= $promoSatuan->kode_voucher; ?></h1>
                             <p>
                                 Use promo code
                                 <strong><?= $promoSatuan->kode_voucher; ?></strong>
@@ -31,24 +31,7 @@
                 </div>
             </div>
         <?php endforeach ?>
-        <div class="single-hero-items set-bg" data-setbg="/img/hero-2.jpg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-5">
-                        <span>Bag,kids</span>
-                        <h1>Black friday</h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore
-                        </p>
-                        <a href="#" class="primary-btn">Shop Now</a>
-                    </div>
-                </div>
-                <div class="off-card">
-                    <h2>Sale <span>50%</span></h2>
-                </div>
-            </div>
-        </div>
+
     </div>
 </section>
 <!-- Hero Section End -->
