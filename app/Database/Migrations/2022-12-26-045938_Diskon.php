687e14331066c92abbf0d7e100f756860d8dba00
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-12-26 12:23:22 +0700

    add fields for diskon table

--- app/Database/Migrations/2022-12-26-045938_Diskon.php
+++ app/Database/Migrations/2022-12-26-045938_Diskon.php
@@ -9,10 +9,46 @@ class Diskon extends Migration
     public function up()
     {
         //
+        $this->forge->addField([
+            "id" => [
+                "type" => "INT",
+                "constraint" => 11,
+                "auto_increment" => TRUE,
+            ]
+        ]);
+        $this->forge->addField([
+            "kode_voucher" => [
+                "type" => "VARCHAR",
+                "constraint" => 10,
+            ]
+        ]);
+        $this->forge->addField([
+            "tanggal_mulai_berlaku" => [
+                "type" => "DATE",
+            ]
+        ]);
+        $this->forge->addField([
+            "tanggal_akhir_berlaku" => [
+                "type" => "DATE",
+            ]
+        ]);
+        $this->forge->addField([
+            "besar_diskon" => [
+                "type" => "TINYINT",
+                "constraint" => 2,
+            ]
+        ]);
+        $this->forge->addField([
+            "aktif" => [
+                "type" => "TINYINT",
+                "constraint" => 1,
+            ]
+        ]);
     }
 
     public function down()
     {
         //
+        $this->forge->dropTable("diskon");
     }
 }
